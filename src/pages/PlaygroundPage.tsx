9d38dc8f59f2819615199c5d6e1d02ed7ce74728
podestal <luisrodriguezpodesta@gmail.com>
2024-09-09 13:07:35 -0400

    testing expandable text

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,9 +1,10 @@
 // import Onboarding from "../components/Onboarding";
-import TermsAndConditions from "../components/TermsAndConditions";
+// import TermsAndConditions from "../components/TermsAndConditions";
+import ExpandableText from "../components/ExpandableText";
 
 const PlaygroundPage = () => {
   // return <Onboarding />;
-  return <TermsAndConditions />
+  return <ExpandableText text="short text"/>
 };
 
 export default PlaygroundPage;
---
+++ tests/components/ExpandableText.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react'
+import ExpandableText from '../../src/components/ExpandableText'
+
+describe('ExpandableText', () => {
+    it('should render the full text if less than 255 characters', () => {
+        const text = 'short text'
+        render(<ExpandableText text={text}/>)
+        expect(screen.getByText(text)).toBeInTheDocument()
+    })
+    it('should truncate text if longer than 255 characters', () => {
+        const text = 'a'.repeat(256)
+        render(<ExpandableText text={text}/>)
+        const truncatedText = text.substring(0, 255) + '...'
+        expect(screen.getByText(truncatedText)).toBeInTheDocument()
+        const button = screen.getByRole('button')
+        expect(button).toBeInTheDocument()
+        expect(button).toHaveTextContent(/more/i)
+    })
+})
