4f5fdb460b55261807a1a0047abe9ab792305567
podestal <luisrodriguezpodesta@gmail.com>
2024-09-10 10:20:13 -0400

    simplyfing testing

--- tests/components/ExpandableText.test.tsx
+++ tests/components/ExpandableText.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react'
 import ExpandableText from '../../src/components/ExpandableText'
 
 describe('ExpandableText', () => {
+
     it('should render the full text if less than 255 characters', () => {
         const text = 'short text'
         render(<ExpandableText text={text}/>)
